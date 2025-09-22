2c45d94f782f686a3f3c514c6a5014201ee7371f
Kurorirorira <Omi20081007@icloud.com>
2025-09-22 21:45:07 +0900

    Update page.tsx

--- SeikoFes/seikofes/src/app/FunnyNews4/page.tsx
+++ SeikoFes/seikofes/src/app/FunnyNews4/page.tsx
@@ -16,7 +16,7 @@ export default function FunnyNews4(){
                 <p>当日来場時に紙でも貰っていただけるとうれしいです！！！</p>
                 </>
             }
-            image="/images/ニュース３.webp"
+            image="/images/IMG_3030.jpeg"
             date="21:24・2025年9月10日・87件の表示"
              />
         </div>
